f785e21747f7758b06bd07ec60bdce82cd5beb07
Thiago Mônaco <thiagomonaco98@gmail.com>
2024-02-18 16:52:30 -0300

    adding exercise 26 solution

---
+++ leetcode-26.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const removeDuplicates = function(nums) {
+    let k = 1
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i] !== nums[i-1]) {
+            nums[k++] = nums[i]
+        }
+    }
+    return k
+};
